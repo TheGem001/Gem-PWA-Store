01957d87d7b25b32604916c06110111dcfa4cc66
Muhammad Saqib Zahid <m.saqibzahid.iqbal@gmail.com>
2025-12-17 15:43:34 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-const CACHE_NAME = 'gem-store-v1.2';
+
+/**
+ * Gem Store Service Worker (v1.3)
+ */
+
+const CACHE_NAME = 'gem-store-v1.3';
 const ASSETS = [
   './',
   './index.html',
@@ -7,11 +12,18 @@ const ASSETS = [
   './assets/splitify.svg',
   './assets/gem-logo.svg',
   'https://cdn.tailwindcss.com',
-  'https://unpkg.com/lucide@latest'
+  'https://unpkg.com/lucide@latest',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
